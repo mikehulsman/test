a793e9b37d5f9f10d67c63b470784172f5e69603
Mike <mike@hulsman.net>
2018-11-09 11:12:50 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-  "Hello, Sinatra good to see you all @Rotterdam! From germany""kdfkidkdf"
+  "Hello, Sinatra good to see you all @Rotterdam! From germany""kdfkidkdfmklfkefwjkwefkoweklfklweew"
 end
